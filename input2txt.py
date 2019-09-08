0848abd85a88fc2912b43dfcfbbc9028e753ac19
Deshar <deshar_wor@yahoo.com>
2019-09-08 13:22:05 +0700

    1st commit

--- input2txt.py
+++ input2txt.py
@@ -1,2 +1,28 @@
 import csv
-print "test"
+
+def main():
+    x = open('Master Config.txt', 'r')
+    y = open('TEST_CSV.csv', 'r')
+    reader = csv.reader(y, delimiter=',')
+
+    for k in reader:
+        print (k[0])
+        z = open('{0}.txt'.format(k[0]),'w')
+        line = 0
+
+
+
+
+
+    x.close()
+    y.close()
+    z.close()
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
